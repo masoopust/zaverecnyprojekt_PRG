8bc0af354e38fb29a55bb99eae8ab3b63d4914ff
masso <patrikmasopust@seznam.cz>
2024-05-27 12:00:41 +0200

    optimization

--- ninjagame/scripts/tilemap.py
+++ ninjagame/scripts/tilemap.py
@@ -32,12 +32,20 @@ class Tilemap:
 
     def render(self, surf, offset = (0, 0)):
         for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])      #offset odečítám, protože kameraq se pohne doprava tak vše na screenu se pohne doleva
+            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])      #offset odečítám, protože kamera se pohne doprava tak vše na screenu se pohne doleva
 
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):  # chceme renderovat pouze pixely co jsou  na obrazovce podle pozice kamery, kterou zde zjišťujeme pomocí offset[0] // self.tile_size --> tím zjistíme levý horní pixel
+            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                loc = str(x) + ';' + str(y)
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    surf.blit(self.game.assets[tile['type']][tile['variant'] - 1],(tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+
+       """ 
         for loc in self.tilemap:
             tile = self.tilemap[loc]
             surf.blit(self.game.assets[tile['type']][tile['variant']-1], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
-
+        """""
 
 
 
